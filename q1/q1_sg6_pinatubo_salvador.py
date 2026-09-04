5d95432a8107243f94682054f319988aa039f05e
Beatrice <bdsalvador2030@clc.pshs.edu.ph>
2026-09-04 15:47:00 +0800

    Implement Lab and Technician classes
    Added Lab and Technician classes with lab assignment functionality.

--- q1/q1_sg6_pinatubo_salvador.py
+++ q1/q1_sg6_pinatubo_salvador.py
@@ -1,1 +1,16 @@
+class Lab:
+  def __init__(self, roomNumber):
+    self.roomNumber = roomNumber
 
+
+class Technician:
+  assigned_lab = None
+  def __init__(self, name):
+    self.name = name
+  def assign_lab(self, lab_obj):
+    self.assigned_lab = lab_obj
+
+chem_lab = Lab("302")
+mr_cruz = Technician("Mr. Cruz")
+mr_cruz.assign_lab(chem_lab)
+print(mr_cruz.assign_lab.room_number)
