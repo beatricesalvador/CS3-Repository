0b979f926955fa5e744d8a71f3cedfdd8e496295
Beatrice <bdsalvador2030@clc.pshs.edu.ph>
2026-09-04 16:27:36 +0800

    Implement Hero class with damage handling
    Added a Hero class with methods for taking damage and displaying HP.

--- q1/q1_sg5_a1_pinatubo_salvador.py
+++ q1/q1_sg5_a1_pinatubo_salvador.py
@@ -1,1 +1,14 @@
+class Hero:
+  def __init__(self, name, hp = 100):
+    self.name = name
+    self.hp = hp
+  def take_damage(self, amount):
+    self.hp -= amount
+    print(f"{self.name} has taken {amount} damage and their remaining HP is {self.hp}")
+
+hero1 = Hero("Arthur")
+hero1.take_damage(10)
+hero2 = Hero("Morgana")
+hero2.take_damage(0)
+
 
